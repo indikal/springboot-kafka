9f72ba03bcaa808772ed9d9737a39217a97296c8
Indika Liyanage <indika.liyanage@ebuilder.com>
2018-08-15 16:41:28 +0530

    Organized imports

--- src/main/java/lk/inli/springboot/RestJsonController.java
+++ src/main/java/lk/inli/springboot/RestJsonController.java
@@ -5,7 +5,6 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lk.inli.springboot.model.Student;
